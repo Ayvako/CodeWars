cd398bc067e4906fcc4411d5ea567e46fabac3a8
Ayvako <ayvako2022@gmail.com>
2023-01-06 19:29:23 +0200

    06.01.2023

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ArrSum(new double[] {1.5,-0.4} ));
+
+            Console.WriteLine(FindShort("bitcoin take over the world maybe who knows perhaps"));
+
         }
 
 
@@ -44,10 +46,87 @@
 
         public string Rps(string p1, string p2)
         {
-            return "";
+
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>
+            {
+
+                {"scissors paper","Player 1 won!"},
+                {"paper scissors","Player 2 won!"},
+
+                {"rock scissors","Player 1 won!"},
+                {"scissors rock","Player 2 won!"},
+
+                {"paper rock","Player 1 won!"},
+                {"rock paper","Player 2 won!"},
+
+            };
+
+
+            if (p1 == p2)
+                return "Draw!"; 
+            
+
+
+            return keyValuePairs[p1 + " "+ p2];
+        }
+
+        public static string AbbrevName(string name)
+        {
+            var names = name.ToUpper().Split('.',' ');
+            return $"{names[0][0]}.{names[1][0]}";
+        }
+
+        public static int Multiply(int x)
+        {
+
+            return  x * ( x % 2 == 0 ? 8:9 );
+        }
+        public static int[] InvertValues(int[] input)
+        {
+
+            return input.Select(a=>-a).ToArray();
+
         }
 
 
 
+        public static bool Hero(int bullets, int dragons)
+        {
+            return dragons * 2 <= bullets;
+        }
+
+        public static int FindSmallestInt(int[] args)
+        {
+            return args.Min();
+        }
+
+        public static int Past(int h, int m, int s)
+        {
+            return (h * 60 * 60 + m * 60 + s) * 1000;
+        }
+
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            var peopleIn = 0;
+            var peopleOut = 0;
+            foreach(var v in peopleListInOut)
+            {
+                peopleIn += v[0];
+                peopleOut += v[1];
+            }
+
+           
+            return peopleIn - peopleOut;
+        }
+
+        public static int FindShort(string s)
+        {
+            return s.Split(" ").Min(x => x.Length);
+        }
+
+        public static int[] Maps(int[] x)
+        {
+            return x;
+        }
     }
 }
