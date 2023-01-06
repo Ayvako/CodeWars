951ef43fb2296edcf696775ae3922a134ca6c3d4
Ayvako <ayvako2022@gmail.com>
2023-01-06 19:29:23 +0200

    06.01.2023

--- Program.cs
+++ Program.cs
@@ -15,7 +15,39 @@
 
         public static int StringToNumber(String str)
         {
-            //TODO: Convert str into a number
+          return  Convert.ToInt32(str);
+
         }
+
+        public static string RepeatStr(int n, string s)
+        {
+
+            string result = "";
+            for (int i = 0; i < n; i++)
+                result += s;
+            return result;
+        }
+
+        public static string Bmi(double weight, double height)
+        {             
+            double bmi = weight / (height * height);
+            if (bmi <= 18.5) return "Underweight";
+
+            if (bmi <= 25.0) return "Normal";
+
+            if (bmi <= 30.0) return "Overweight";
+
+            if (bmi > 30) return "Obese";
+
+            return "";
+        }
+
+        public string Rps(string p1, string p2)
+        {
+            return "";
+        }
+
+
+
     }
 }
