de5fbc3ed31d124908fbb141def64bb17e557983
Cat <Cat@CAT->
2023-01-06 19:07:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArrSum(new double[] {1.5,-0.4} ));
+        }
+
+
+        static double ArrSum(double[] arr)
+        {
+            return arr.Sum();
+        }
+
+        public static int StringToNumber(String str)
+        {
+            //TODO: Convert str into a number
+        }
+    }
+}
